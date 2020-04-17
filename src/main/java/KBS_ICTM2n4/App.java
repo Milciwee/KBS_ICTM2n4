b8d2e634705c4e30e3e48574a47c4cd3530d7c87
Milco <55544948+Milciwee@users.noreply.github.com>
2020-04-17 11:10:18 +0200

    project structure

---
+++ src/main/java/KBS_ICTM2n4/App.java
@@ -0,0 +1,17 @@
+package KBS_ICTM2n4;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
