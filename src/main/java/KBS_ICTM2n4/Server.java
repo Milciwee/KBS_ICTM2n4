7d4269da24d9e890ebee2cc72742d8f3d8fdf36e
Milco <55544948+Milciwee@users.noreply.github.com>
2020-04-17 15:54:23 +0200

    Server class

---
+++ src/main/java/KBS_ICTM2n4/Server.java
@@ -0,0 +1,47 @@
+package KBS_ICTM2n4;
+
+public class Server {
+
+  private String type;
+  private String name;
+  private double availabilty;
+  private double price;
+
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getAvailabilty() {
+    return this.availabilty;
+  }
+
+  public void setAvailabilty(double availabilty) {
+    this.availabilty = availabilty;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+
+  
+  
+
+}
