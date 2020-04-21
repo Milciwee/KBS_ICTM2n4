e59abf599ed9e93f1a2736628e8842c9c756c5d8
ruben <rubenvdb-work@outlook.com>
2020-04-21 13:54:07 +0100

    DisplayGraphics aangemaakt

---
+++ src/main/java/KBS_ICTM2n4/DisplayGraphics.java
@@ -0,0 +1,13 @@
+package KBS_ICTM2n4;
+
+import java.awt.*;
+
+public class DisplayGraphics extends Canvas {
+    public void paint(Graphics g){
+        setBackground(Color.blue);
+
+    }
+    public void drawWs1(int y, int x){
+
+    }
+}
--- src/main/java/KBS_ICTM2n4/Screen.java
+++ src/main/java/KBS_ICTM2n4/Screen.java
@@ -28,6 +28,11 @@ public class Screen extends JFrame implements ActionListener {
     static JTextField jtfOptimizeAnswer = new JTextField();
     static JLabel jlDesignName = new JLabel("");
     static JComboBox dropdowndesign;
+    JButton jbCalculate = new JButton("Calculate");
+    JButton jbOptimize = new JButton("Optimize");
+    JButton jbDelete = new JButton("Delete");
+    JButton jbSave = new JButton("Save");
+    JButton jbSaveAs = new JButton("Save as new Design");
 
     public Screen(){
         //titel van de window
@@ -60,51 +65,43 @@ public class Screen extends JFrame implements ActionListener {
         dropdownedit.setBounds(525,0,150,25);
         JLabel jlDesnameEdit = new JLabel("Design name:");
         jlDesnameEdit.setBounds(10,20,100,25);
-
         //labels
-        JLabel jlWs1 = new JLabel("Webserver 1");
-        JLabel jlWs2 = new JLabel("Webserver 2");
-        JLabel jlWs3 = new JLabel("Webserver 3");
         JLabel jlDbs1 = new JLabel("Database server 1");
         JLabel jlDbs2 = new JLabel("Database server 2");
         JLabel jlDbs3 = new JLabel("Database server 3");
+        JLabel jlWs1 = new JLabel("Webserver 1");
+        JLabel jlWs2 = new JLabel("Webserver 2");
+        JLabel jlWs3 = new JLabel("Webserver 3");
         JLabel jlFw = new JLabel("Pfsense");
         JLabel jlAvailability = new JLabel("Availibility");
         //label bounds
-        jlWs1.setBounds(10,60,120,25);
-        jlWs2.setBounds(10,90,120,25);
-        jlWs3.setBounds(10,120,120,25);
-        jlDbs1.setBounds(10,150,120,25);
-        jlDbs2.setBounds(10,180,120,25);
-        jlDbs3.setBounds(10,210,120,25);
+        jlDbs1.setBounds(10,60,120,25);
+        jlDbs2.setBounds(10,90,120,25);
+        jlDbs3.setBounds(10,120,120,25);
+        jlWs1.setBounds(10,150,120,25);
+        jlWs2.setBounds(10,180,120,25);
+        jlWs3.setBounds(10,210,120,25);
         jlFw.setBounds(10,240,120,25);
         jlAvailability.setBounds(10,330,100,25);
-
-        //buttons
-        JButton jbCalculate = new JButton("Calculate");
-        JButton jbOptimize = new JButton("Optimize");
-        JButton jbDelete = new JButton("Delete");
-        JButton jbSave = new JButton("Save");
-        JButton jbSaveAs = new JButton("Save as new Design");
         //button bounds
         jbCalculate.setBounds(10,280,100,25);
         jbOptimize.setBounds(140,330,100,25);
         jbDelete.setBounds(70,500,100,25);
         jbSave.setBounds(270,500,100,25);
         jbSaveAs.setBounds(470,500,150,25);
-
         //textfield bounds
         jtfDesnameEdit.setBounds(110,20,100,25);
-        jtfWs1.setBounds(140,60,25,25);
-        jtfWs2.setBounds(140,90,25,25);
-        jtfWs3.setBounds(140,120,25,25);
-        jtfDb1.setBounds(140,150,25,25);
-        jtfDb2.setBounds(140,180,25,25);
-        jtfDb3.setBounds(140,210,25,25);
+        jtfDb1.setBounds(140,60,25,25);
+        jtfDb2.setBounds(140,90,25,25);
+        jtfDb3.setBounds(140,120,25,25);
+        jtfWs1.setBounds(140,150,25,25);
+        jtfWs2.setBounds(140,180,25,25);
+        jtfWs3.setBounds(140,210,25,25);
         jtfCalculateAnswer.setBounds(120,280,300,25);
         jtfavailability.setBounds(80,330, 40,25);
         jtfOptimizeAnswer.setBounds(10,370,300,25);
-
+        //actionlisteneners
+        jbOptimize.addActionListener(this);
         //toevoegen aan panel
         editPanel.add(dropdownedit);
         editPanel.add(jlDesnameEdit);
@@ -133,43 +130,38 @@ public class Screen extends JFrame implements ActionListener {
         editPanel.add(jtfOptimizeAnswer);
 
         //designpanel
+        //dropdown
         dropdownitemsdesign = dropdownitemsedit;
         dropdownitemsdesign.add("Add new Design");
         dropdowndesign = new JComboBox(dropdownitemsdesign.toArray());
         dropdowndesign.setBounds(525,0,150,25);
         dropdowndesign.addActionListener(this);
+        //graphics
+        DisplayGraphics graphicsPanel = new DisplayGraphics();
+        graphicsPanel.setBounds(10,250,660,270);
+        //variabele JLabels
         jlDesignName.setText("Design name: " + dropdowndesign.getSelectedItem());
+        //statische JLabels
+        JLabel jlConfiguration = new JLabel("Configuration:");
+        JLabel jlFirewall = new JLabel("PFsense");
         jlDesignName.setBounds(10,20,250,25);
+        jlConfiguration.setBounds(10,50,100,25);
+        //for loop waarin door de lijst met opgeslagen servers wordt gegaan om deze onder elkaar te krijgen.
+
+
+
+
 
         designPanel.add(dropdowndesign);
         designPanel.add(jlDesignName);
-
-
+        designPanel.add(jlConfiguration);
+        designPanel.add(graphicsPanel);
 
         //voegt de panes toe aan tabbedpane met een name
         tabbedPane.addTab("Monitor",monitorPanel);
         tabbedPane.addTab("Edit",editPanel);
         tabbedPane.addTab("Design",designPanel);
         add(tabbedPane);
-        //oude code
-//        mainPanel = new JPanel();
-//        mainPanel.setLayout(null);
-//        JButton jbDesign = new JButton("Design");
-//        jbDesign.setBounds(100,0,100,25);
-//        designPanel.add(jbDesign);
-//        jbMonitor = new JButton("Monitoring");
-//        jbMonitor.setBounds(0,0,100,25);
-//        jbEdit = new JButton("Edit");
-//        jbEdit.setBounds(100,0,100,25);
-//        jbDesign = new JButton("Design");
-//        jbDesign.setBounds(200,0,100,25);
-//
-//        //componenten toevoegen + eventlistener
-//        mainPanel.add(jbMonitor);
-//        mainPanel.add(jbEdit);
-//        mainPanel.add(jbDesign);
-//        add(mainPanel);
-
         //zichtbaarheid aanzetten
         setVisible(true);
     }
@@ -177,6 +169,9 @@ public class Screen extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()== dropdowndesign){
             jlDesignName.setText("Design name: " + dropdowndesign.getSelectedItem());
+        }
+        if (e.getSource() == jbOptimize){
+
         }
     }
 }
