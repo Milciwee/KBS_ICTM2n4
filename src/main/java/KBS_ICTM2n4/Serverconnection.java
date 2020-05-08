5c21ae6d264f5e709d401f308db4bb1b8486b408
Berend <berendvdhoef@gmail.com>
2020-05-08 11:34:17 +0200

    Methode om de uptime van een server terug te krijgen als String toegevoegd

--- src/main/java/KBS_ICTM2n4/Serverconnection.java
+++ src/main/java/KBS_ICTM2n4/Serverconnection.java
@@ -1,16 +1,21 @@
 package KBS_ICTM2n4;
 //dit maakt de verbinding tussen de client en de servers
 
+import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class Serverconnection {
 
-    //145.44.233.16
+    public static Session session;
 
-    public static boolean canConnectWithServer(String DestinationIP) {
+    public static boolean makeConnectionWithServer(String destinationIP) {
 
-        String host = DestinationIP;
+        String host = destinationIP;
         String username = "root"; //root
         String password = "Teamsvmware01!"; //Teamsvmware01!
         int port = 3389; //3389
@@ -18,33 +23,93 @@ public class Serverconnection {
         try {
 
             JSch jsch = new JSch();
-            Session session = jsch.getSession(username, host, port);
+            session = jsch.getSession(username, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
-
             session.connect();
 
             if(session.isConnected()) {
                 System.out.println("Verbinding is geslaagd.");
-                session.disconnect();
                 return true;
             } else {
                 System.out.println("Het is niet gelukt om een verbinding te maken.");
-                session.disconnect();
                 return false;
             }
 
         } catch(Exception e) {
 
-            System.out.println("Onbekende error.");
+            System.out.println("Er is iets misgegaan bij het maken van een verbinding met de server.");
             return false;
 
         }
     }
 
-    public static void main(String[] args) {
-        if(canConnectWithServer("145.44.233.16")) {
-            System.out.println("Dat lukte, blijkbaar.");
+    public static String serverUpTime() {
+
+        //uptime, uptime -s en uptime -p werken
+        String command = "uptime -p\nexit\n";
+
+        try {
+
+            Channel channel = session.openChannel("shell");
+
+            channel.setInputStream(new ByteArrayInputStream(command.getBytes(StandardCharsets.UTF_8)));
+            InputStream in = channel.getInputStream();
+            StringBuilder outBuff = new StringBuilder();
+
+            channel.connect();
+
+            while (true) {
+                for (int c; ((c = in.read()) >= 0); ) {
+                    outBuff.append((char) c);
+                }
+
+                if (channel.isClosed()) {
+                    if (in.available() > 0) continue;
+                    break;
+                }
+            }
+
+            channel.disconnect();
+
+            String[] lines = outBuff.toString().split("\\r?\\n");
+
+            String output = null;
+
+            for (String line : lines) {
+                if (line.startsWith("up ")) {
+                    output = line;
+                }
+            }
+
+            return output;
+
+        } catch (Exception e) {
+
+            System.out.println("Er is iets misgegaan bij het achterhalen van de uptime");
+            return "";
+
         }
+    }
+
+    public static void closeConnectionWithServer() {
+        session.disconnect();
+        session = null;
+    }
+
+    public static void main(String[] args) {
+
+        String upTime = null;
+
+        //145.44.233.16
+
+        if(makeConnectionWithServer("145.44.233.16")) {
+            upTime = serverUpTime();
+            closeConnectionWithServer();
+        }
+
+        System.out.println("---");
+        System.out.println(upTime);
+
     }
 }
