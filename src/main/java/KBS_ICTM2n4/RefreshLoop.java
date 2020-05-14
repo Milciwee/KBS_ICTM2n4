c096a01c32b45bf255c6a25f5e1889430e60beaa
Milco <55544948+Milciwee@users.noreply.github.com>
2020-05-14 14:41:28 +0200

    comments weg

--- src/main/java/KBS_ICTM2n4/RefreshLoop.java
+++ src/main/java/KBS_ICTM2n4/RefreshLoop.java
@@ -2,12 +2,11 @@ package KBS_ICTM2n4;
 
 import java.util.TimerTask;
 
-public class Test extends TimerTask{
+public class RefreshLoop extends TimerTask{
 
   @Override
   public void run() {
     MonitoringDialog.refreshServers();
-    //MonitoringDialog.printConnections();
 
   }
 
