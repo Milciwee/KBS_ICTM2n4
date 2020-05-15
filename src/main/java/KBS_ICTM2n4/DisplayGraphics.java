dace752be88de79164b1e77239f4e1ae9890d640
22kevin <s1146529@student.windesheim.nl>
2020-05-15 12:33:45 +0200

    Paint functie update

--- src/main/java/KBS_ICTM2n4/DisplayGraphics.java
+++ src/main/java/KBS_ICTM2n4/DisplayGraphics.java
@@ -51,11 +51,11 @@ public class DisplayGraphics extends Canvas {
         aWServers1 = serverAmount[3];
         aWServers2 = serverAmount[4];
         aWServers3 = serverAmount[5];
-        //Hierondder initaliseer ik een paar variabelen die gebruikt worden om de rectangle etc te tekenen
+        //Hieronder initaliseer ik een paar variabelen die gebruikt worden om de rectangle etc te tekenen
         DServerY = 360;
         WServerY = 0;
-        wserverIp = 30;
-        dserverIp = 20;
+        wserverIp = 2;
+        dserverIp = 7;
         //
         x=0;
         g.drawImage(FirewallImage,x,0,66,120,this);
@@ -64,15 +64,11 @@ public class DisplayGraphics extends Canvas {
         g.drawString("192.168.1.1",x,20);
         g.drawRect(x,0,66,120+1);
 
-
-        //g.drawLine(66,60,180,60);//lijn van firewall naar groep webservers
-        //g.drawLine(66 /2,120,66/2,DServerY);//lijn van firewall naar groep databaseservers
-
         x+=0;
         //While loops die de Database- en WebServers op het scherm tekent
         i=0;     //initializer voor While-loop
         databaseGroupBegin = x;
-        //g.drawRect(x-10,100,100,100);
+
         while(i < aDatabaseServers1){
             g.drawImage(DServerImage,x,DServerY,66,120,this);//tekent plaatje van de webservers
             g.drawString("DServer 1",x,DServerY+100);
@@ -99,18 +95,13 @@ public class DisplayGraphics extends Canvas {
         databaseGroupEnd=x-14;
         //Hieronder begint het tekenen van een ruimte om de servers heen als er tenminste 1 server is.
         if(aDatabaseServers1 > 0 ||aDatabaseServers2 > 0 ||aDatabaseServers3 > 0){
-           // g.drawLine(66 /2,120+1,66/2,DServerY);//lijn van firewall naar groep databaseservers
-
-
-            //g.drawLine(66 /2,(240-((120/2)/2)) + (120/2+1)/2,170,(240-((120/2)/2)) + (120/2+1)/2);//lijn naar de routerbox vanaf vorige lijn
             g.drawLine(66/2,(240-((120/2)/2)+(120/2+1)),66/2,DServerY);//lijn van midden routerbox naar de database servers
-
             g.drawRect(databaseGroupBegin,DServerY,databaseGroupEnd-databaseGroupBegin,120+1);
-            g.drawString("192.168.1.-",databaseGroupBegin+((databaseGroupEnd-databaseGroupBegin)/2)-30,DServerY+20);
+            g.drawString("192.168.1.-",databaseGroupBegin+((databaseGroupEnd-databaseGroupBegin)/2)-30,DServerY+20);//Ip addres van db servers
         }
         i=0;    //initializer voor While-loop
         x=0 + 180;    //x op nul(+180) zetten voor een nieuwe rij afbeeldingen (web-servers)
-        //int xWebserverInit= x;
+
         webserverGroupBegin = x-10;
         g.drawImage(RouterImage,0,240-((120/2)/2),66,120/2,this);
         g.drawImage(RouterImage,0+80,240-((120/2)/2),66,120/2,this);
@@ -119,11 +110,7 @@ public class DisplayGraphics extends Canvas {
         g.drawString(".2",0,240-((120/2)/2)+60);
         g.drawString(".3",0+80,240-((120/2)/2)+60);
 
-
-        //g.drawString("Router 1",x,(240-((120/2)/2))+50);
-        //g.drawRect(webserverGroupBegin,240-((120/2)/2),(x+80+66)-webserverGroupBegin +10,120/2+1);
         g.drawRect(0,240-((120/2)/2),66+14+66,120/2+1);
-        //g.drawRect(webserverGroupBegin,240-((120/2)/2),(10+66+14+66+10),120/2+1);
 
         while(i < aWServers1){
 
@@ -160,13 +147,11 @@ public class DisplayGraphics extends Canvas {
             g.drawLine(66,60,webserverGroupBegin,60);//lijn van firewall naar groep webservers
             g.drawRect(webserverGroupBegin,WServerY,webserverGroupEnd-webserverGroupBegin,120+1);
            // g.drawString("192.168.1.-",webserverGroupBegin-webserverGroupEnd+webserverGroupEnd + webserverGroupBegin,WServerY+20);
-            g.drawString("192.168.1.-",webserverGroupBegin+((webserverGroupEnd-webserverGroupBegin)/2)-30,WServerY+20);
+            g.drawString("192.168.0.-",webserverGroupBegin+((webserverGroupEnd-webserverGroupBegin)/2)-30,WServerY+20);
         }
 
         i=0;     //initializer voor While-loop
         x = 0;
-
-        //setBackground(Color.blue);
 
 
     }
